6133e725ab17eb81edbc3dc32989bd31c52dfc96
Aaron <astrauli@gmail.com>
2017-11-23 11:54:23 -0800

    made category button

--- frontend/components/categories/category_card_list.jsx
+++ frontend/components/categories/category_card_list.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import CategoryCard from './category_card';
 
-const CategoryCardList = () => {
-  return (
+class CategoryCardList extends React.Component {
+  constructor() {
+    super();
+    this.state = {clicked: null};
+  }
+
+  render() {
+    return (
       <div className="category-list-container">
-      <CategoryCard icon={() => (<i className="fa-2x fa fa-home" aria-hidden="true"></i>)} text="All Events"></CategoryCard>
-      <CategoryCard icon={() => (<i className="fa-2x fa fa-futbol-o" aria-hidden="true"></i>)} text="Sports"></CategoryCard>
-      <CategoryCard icon={() => <i className="fa-2x fa fa-microphone" aria-hidden="true"></i>} text="Concerts"></CategoryCard>
-      <CategoryCard icon={() => (<i className="fa-2x fa fa-smile-o" aria-hidden="true"></i>)} text="Theater &amp; Comedy"></CategoryCard>
-    </div>
-  )
+        <CategoryCard icon={() => (<i className="fa-2x fa fa-home" aria-hidden="true"></i>)} text="All Events"></CategoryCard>
+        <CategoryCard icon={() => (<i className="fa-2x fa fa-futbol-o" aria-hidden="true"></i>)} text="Sports"></CategoryCard>
+        <CategoryCard icon={() => <i className="fa-2x fa fa-microphone" aria-hidden="true"></i>} text="Concerts"></CategoryCard>
+        <CategoryCard icon={() => (<i className="fa-2x fa fa-smile-o" aria-hidden="true"></i>)} text="Theater &amp; Comedy"></CategoryCard>
+      </div>
+    )
+  }
 };
 
 export default CategoryCardList;
