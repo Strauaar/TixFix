7c84fd07ed118167efb5e322e9cfa441060fe068
Aaron <astrauli@gmail.com>
2017-11-30 18:15:34 -0800

    refactor style on user home

--- frontend/components/user/liked_card_item.js
+++ frontend/components/user/liked_card_item.js
@@ -3,7 +3,9 @@ import React from 'react';
 const LikedCardItem = ({item}) => (
   <div className="liked-card">
     <div className="liked-card-image" style={{backgroundImage: `url(${item.img_url})`}}></div>
-    <div></div>
+    <div>
+      
+    </div>
   </div>
 );
 
